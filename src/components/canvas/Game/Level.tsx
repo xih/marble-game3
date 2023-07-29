1ffe9444d99d7965b76e79427fcd8d9ec0c37194
Dennis <dennis@momentum.local>
2023-07-29 11:36:29 -0700

    add indexing to the blocks

--- src/components/canvas/Game/Level.tsx
+++ src/components/canvas/Game/Level.tsx
@@ -1,10 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Physics, RapierRigidBody, RigidBody, quat } from "@react-three/rapier";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import * as THREE from "three"
 
+
+type BlockDefinition = (props: {
+  position: [x: number,y: number, z: number]}) => JSX.Element
 
 const BlockStart = ({ position} : {position: [number, number, number]}) => {
   return (
@@ -19,21 +22,11 @@ const BlockStart = ({ position} : {position: [number, number, number]}) => {
   )
 }
 
-const BlockMiddle = ({ position} : {position: [number, number, number]}) => {
-  return (
-    <>
-      <group position={position}>
-        <mesh scale={[4, 0.2, 4]} receiveShadow>
-          <boxGeometry args={[1, 1, 1]}/>
-          <meshBasicMaterial color="yellowgreen"/>
-        </mesh>
-      </group>
-    </>
-  )
-}
+
 
 
 const BlockEnd = ({ position} : {position: [number, number, number]}) => {
+  const hamburer = useGLTF("./hamburger2.glb")
   return (
     <>
       <group position={position}>
@@ -41,39 +34,74 @@ const BlockEnd = ({ position} : {position: [number, number, number]}) => {
           <boxGeometry args={[1, 1, 1]}/>
           <meshBasicMaterial color="limegreen"/>
         </mesh>
+        <primitive object={hamburer.scene} scale={0.2}/>
       </group>
     </>
   )
 }
 
 
-
-const BlockSpinner = ({ position}: {position: [number, number, number ]}) => {
-
-
+const BlockSpinner = ({ position}: {position: [number, number, number ]}): JSX.Element => {
   const blockRef = useRef<RapierRigidBody>(null!)
-
-
   useFrame((state, delta) => {
     const time = state.clock.getElapsedTime()
-    // get the euler rotation 
-
     const eulerRotation = new THREE.Euler(0, time, 0)
     const quaternionRotation = new THREE.Quaternion()
     quaternionRotation.setFromEuler(eulerRotation)
-  
     blockRef.current.setNextKinematicRotation(quaternionRotation)
 
   })
 
 
-  return <>
-  <RigidBody type="kinematicPosition" ref={blockRef} >
+  return <group position={position}>
+ <mesh scale={[4, 0.2, 4]} receiveShadow>
+          <boxGeometry args={[1, 1, 1]}/>
+          <meshBasicMaterial color="yellowgreen"/>
+        </mesh>
+  <RigidBody type="kinematicPosition" ref={blockRef}  >
     <mesh scale={[4,0.2,1]} position={[0, 0.5, 0]}>
       <boxGeometry />
       <meshStandardMaterial color="red"/>
     </mesh>
   </RigidBody>
+  </group>
+}
+
+const BlockGate = ({ position}: {position: [number, number, number ]}): JSX.Element => {
+  const obstacle = useRef<RapierRigidBody>(null!)
+  const [timeOffset] = useState<number>(() => Math.random() * 2 * Math.PI)
+
+  useFrame((state, delta) => {
+    const time = state.clock.getElapsedTime()
+    // const eulerRotation = new THREE.Euler(0, time, 0)
+    // const quaternionRotation = new THREE.Quaternion()
+    // quaternionRotation.setFromEuler(eulerRotation)
+
+    const y = Math.sin(time + timeOffset) + 1.15
+
+    const translationVector = new THREE.Vector3(0, Math.floor(time) + 2, 0)
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + y,
+      z: position[2]
+    })
+
+  })
+
+
+  return <>
+  <group position={position} >
+  <mesh scale={[4, 0.2, 4]} receiveShadow>
+          <boxGeometry args={[1, 1, 1]}/>
+          <meshBasicMaterial color="yellowgreen"/>
+        </mesh>
+  <RigidBody type="kinematicPosition" ref={obstacle} >
+    <mesh scale={[4,0.2,1]} position={[0, 0.5, 0]}>
+      <boxGeometry />
+      <meshStandardMaterial color="red"/>
+    </mesh>
+  </RigidBody>
+  </group>
   </>
 }
 
@@ -82,9 +110,20 @@ const Level = () => {
 
   const numBlocks = 5
 
+  const blocks: BlockDefinition[] = []
 
-  // make 5 
-  // const Arr = [].
+  const types = [BlockSpinner, BlockGate]
+
+  for (let i = 0; i < numBlocks; i++ ) {
+    // console.log(Math.floor(Math.random() * types.length))
+    const type = types[Math.floor(Math.random() * types.length)]
+    if(!!type) {
+      blocks.push(type)
+    }
+  }
+
+
+  console.log(blocks)
 
   return (
     <>
@@ -93,9 +132,10 @@ const Level = () => {
       <ambientLight intensity={1.5} />
       <Physics>
         <BlockStart position={[0,0,0]}/>
-        <BlockMiddle position={[0, 0, -4]}/>
-        <BlockSpinner position={[0, 0, -4]}/> 
-        <BlockEnd position={[0,0,-8]}/>
+        {blocks.map((Block, index) => {
+           return <Block key={index} position={[0,0, -(index + 1) * 4]} />
+        })}
+        <BlockEnd position={[0,0,(numBlocks + 1) * -4]}/>
       </Physics>
     </>
   );
