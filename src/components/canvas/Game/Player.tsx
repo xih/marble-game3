6d2edce8832f5b2d64e6ea920ebcc26883662a61
Dennis <dennis@momentum.local>
2023-07-30 15:32:09 -0700

    get jumping working using useEffect

--- src/components/canvas/Game/Player.tsx
+++ src/components/canvas/Game/Player.tsx
@@ -1,18 +1,49 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { RapierRigidBody, RigidBody } from "@react-three/rapier";
+import { RapierRigidBody, RigidBody, useRapier } from "@react-three/rapier";
 import { useControls } from "leva";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const Player = () => {
   const ball = useRef<RapierRigidBody>(null!);
-
   const [subscribeKeys, getKeys] = useKeyboardControls();
+  const { rapier, world } = useRapier();
 
   const { impulseKnob, torqueKnob } = useControls({
     impulseKnob: { value: 0.6, min: 0.1, max: 1, step: 0.1 },
     torqueKnob: { value: 0.2, min: 0.1, max: 1, step: 0.1 },
   });
+
+  const jump = () => {
+    const origin = ball.current.translation();
+    origin.y -= 0.31;
+    const direction = { x: 0, y: -1, z: 0 };
+
+    const ray = new rapier.Ray(origin, direction);
+    const hit = world.castRay(ray, 10, true);
+
+    if (hit.toi < 0.15) {
+      ball.current.applyImpulse({ x: 0, y: 0.5, z: 0 }, true);
+    }
+  };
+
+  // JUMP
+  useEffect(() => {
+    const unsubscribeJump = subscribeKeys(
+      (state) => {
+        return state.jump;
+      },
+      (value) => {
+        if (value) {
+          jump();
+        }
+      }
+    );
+
+    return () => {
+      unsubscribeJump();
+    };
+  }, []);
 
   useFrame((state, delta) => {
     const { forward, backward, leftward, rightward } = getKeys();
